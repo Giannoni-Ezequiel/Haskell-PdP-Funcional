1611716229175b946b418ec9ca5f454b8a430f5a
Giannoni-Ezequiel <egiannoni@frba.utn.edu.ar>
2024-04-10 20:43:12 -0300

    Funciones
    primeras funciones en haskell

---
+++ funcionesIniciales.hs
@@ -0,0 +1,56 @@
+cuadrado::Float -> Float
+cuadrado x = x * x
+cuadrado 1 = 1
+cuadrado 2 = 4
+cuadrado 3 = 9
+
+
+doble,triple::Float -> Float
+doble x = x + x
+triple x = x * 3
+
+esCero::Int -> Bool
+esCero 0 = True
+esCero 1 = False
+
+apellido::String -> String
+apellido "juan" = "perez"
+apellido alguien = "sin apellido"
+
+funcion x = cuadrado x + triple x
+
+inicial::String -> Char
+inicial palabra = head palabra
+
+
+--pi = 3.14
+superficie:: Float -> Float
+superficie radio = pi * cuadrado radio
+
+esPar,esImpar:: Int -> Bool
+--esPar x = (0 || multiploDe2 (x)) == mod 2 x
+esPar x = 0 == mod x 2
+esImpar x = 1 == mod x 2
+
+multiploDe2,multiploDe3::Int -> Int
+multiploDe2 x = x * 2
+multiploDe3 x = x * 3
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial 1 = 1
+factorial x = x * factorial(x - 1)
+
+--pesoPino,metrosRestantes,pesoPinoMenosDe3:: Int -> Int
+--pesoPinoHasta3 altura = altura*300
+--pesoPinoMasDe3:: Int -> Int
+--pesoPinoMasDe3 altura>3 = altura*200+300
+--pesoPino peso = peso*200
+pesoPinoMenosDe3 altura = min 3 altura
+metrosRestantes altura = max 0 (altura - 3)
+pesoPino altura = pesoPinoMenosDe3 * 300 + metrosRestantes * 200
+--esPesoUtil:: Int -> Bool
+
+
+--esBisiesto:: Int -> Bool
+--esBisiesto anio = (esDivisible anio 4) && not(esDivisible anio 100)
